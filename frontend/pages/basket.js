2c032d1d9796165585e2b155899453d33a235625
leoerim10 <63184731+leoerim10@users.noreply.github.com>
2023-03-22 20:18:05 +0100

    basket producs with image added including the checkout button

--- frontend/pages/basket.js
+++ frontend/pages/basket.js
@@ -2,81 +2,77 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 
 export default function Basket({ basket, products }) {
+  const handleRemoveFromBasket = async (el) => {
+    await removeFromBasket(el);
+    window.location.reload();
+  };
+
+
   return (
     <div className='bg-gray-100'>
     <Navbar></Navbar>
     <main className=" flex flex-col max-w-7xl mx-auto sm:flex-row ">
-    <div className='flex-grow m-6 shadow-lg'>
-    <div className=' flex shadow-lg py-1 my-5'> Your Basket </div>
-    <div className=' flex flex-col px-1 space-y-10'>
-    
-            
-            {
-               
-                  <div className='sm:flex justify-between mx-5 space-x-10'  >
-                  <div className='flex flex-grow flex-col'>
-                    { products.length>0 &&<div>
-                     {
-                        products.map(el => {
-                          return(
-                            <div>
-                            <div>
-                              {el.name}
-                            </div>
-                            <div>
-                              {el.description}
-                            </div>
-                            <div>
-                              {el.price}
-                            </div>
-                            <div>
-                              {el.image}
-                            </div>
-                            <div className='flex flex-col justify-between py-3 mx-5'> 
-                              <buttom className =' flex flex-grow-0 m-2 p-1 bg-red-300 rounded-lg justify-center text-sm cursor-pointer hover:bg-red-400 hover:shaodw-lg '
-                              onClick={() => {
-                                  removeFromBasket(el)
-                                }
-                              }
-                              > Remove from Basket</buttom>
-                            </div>
-                            </div>
-
-                          )
-                        })
-                      } 
-                      </div>}
-                  </div>
-                 
+      <div className='flex-grow m-6 shadow-lg'>
+        <div className=' flex shadow-lg py-1 my-5' style={{ fontWeight: 'bold' }}> Your Basket </div>
+        <div className=' flex flex-col px-1 space-y-10'>
+          {products.length > 0 ? (
+            <div className='sm:flex justify-between mx-5 space-x-10'  >
+              <div className='flex flex-grow flex-col'>
+                {products.map(el => {
+                  return (
+                    <div style={{ marginBottom: '20px' }}>
+                      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                        <img src={el.image} alt={el.name} style={{ width: '200px', height: 'auto', marginRight: '20px' }} />
+                        <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+                          <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+                            {el.name}
+                          </div>
+                          <div style={{ fontWeight: 'bold', marginBottom: '5px' }}> 
+                            €{el.price}
+                          </div>
+                        </div>
+                        <div className='flex flex-col justify-between py-3'> 
+                          <button className ='flex flex-grow-0 m-2 p-1 bg-red-300 rounded-lg justify-center text-sm cursor-pointer hover:bg-red-400 hover:shadow-lg'
+                            onClick={() => handleRemoveFromBasket(el)}
+                          > Remove from Basket</button>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div> 
+            </div>
+          ) : (
+            <div style={{ textAlign: 'center', margin: '50px' }}>
+              <p style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>Your basket is empty!</p>
+              {/* <img src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg" alt="empty basket" style={{ width: '200px', height: 'auto' }} /> */}
+            </div>
+          )}
+        </div>
+      </div>
+      {products.length > 0 &&
+        <div className='flex flex-col justify-center items-center flex-grow m-6 shadow-lg'> 
+          <div className='flex mb-2'>Total</div>
+          <div style={{ fontWeight: 'bold' }} className='flex mb-4'>{basket ? `€${basket.totalPrice.toFixed(2)}` : "-"}</div>
+          <div className='flex flex-col items-center justify-center'>
+            <button className ='flex-shrink-0 px-4 py-2 text-sm font-medium text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'>
+              Proceed to Checkout
+            </button>
           </div>
-           
-           
-            }
-    </div>
-    </div>
-    <div className='flex justify-center space-x-5 flex-grow m-6 shadow-lg'> 
-    <div className='flex'>Total</div>
-    <div className='flex'>{basket?basket.totalPrice:"-"}</div>
-    </div>
-  </main>
-    </div>
+        </div>
+      }
+    </main>
+  </div>
+  
   )
 }
 
 
  async function removeFromBasket(prod) {
-  /*
-  find relevant basket
-  parse products string to list of ints
-  subtract price of item from total price
-  create a new string with all products except the removed from basket one
-  create a new basket object with same customer Id but updated prodcuts and totalPrice
-  PUT
-  
-  */
+ 
   let custId = 90
   let products = []
-  const res = await  fetch("http://localhost:8091/baskets/")
+  const res = await  fetch("http://localhost:8090/baskets/")
   console.log("res")
   console.log(res)
   const b = await res.json()
@@ -86,9 +82,6 @@ export default function Basket({ basket, products }) {
 
   let baskett= (b.filter(b => b.customerId === custId))
   
-
-
-
   if(baskett.length >0){
     basket = (baskett[0])
     //console.log(basket)
@@ -111,7 +104,8 @@ export default function Basket({ basket, products }) {
       "products": prodStr
     })
 
-    console.log(updatedBasket)
+    console.log("customer id")
+    console.log(basket.customerId)
 
 
     const response = await fetch(`http://localhost:8090/baskets/${basket.customerId}`, {
@@ -124,39 +118,11 @@ export default function Basket({ basket, products }) {
   }  
 }  
 
-
-/* const removeFromBasket = async(productId) => {
-  let custId = 90;
-  const res = await fetch("http://localhost:8091/baskets/");
-  const baskets = await res.json();
-  const basket = baskets.find(b => b.customerId === custId);
-  
-  if (basket) {
-    const productIds = basket.products.split(",").map(id => parseInt(id));
-    const index = productIds.indexOf(productId);
-    if (index > -1) {
-      productIds.splice(index, 1);
-    }
-    const response = await fetch(`http://localhost:8090/baskets/${custId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        products: productIds.join(",")
-      })
-    });
-  }
-} */
-
-
-
-
 export async function getServerSideProps() {
   // Fetch data from external API
   let custId = 90
   let products = []
-  const res = await  fetch("http://localhost:8091/baskets/")
+  const res = await  fetch("http://localhost:8090/baskets/")
   
   let basket = {}
   if(res.status !==404){
@@ -169,19 +135,11 @@ export async function getServerSideProps() {
       basket = (baskett[0])
       //console.log(basket)
       let prodNrs = basket?.products.split(',').map(el => parseInt(el))
-      const prodDetails = await fetch("http://localhost:8091/products/")
+      const prodDetails = await fetch("http://localhost:8087/products/")
       const allProds = await prodDetails.json()
       products = allProds.filter(el => prodNrs.includes(el.id))
     }
   }
-
-
-
-
-  //const c = await prodDetails.json()
-  //products.push(c)
-  //console.log(products)
-
 
   // Pass data to the page via props
   return { props: { basket, products } }
