1a6368bd5b153ed1ed4b8d8dcf4550d9d19d7b32
leoerim10 <63184731+leoerim10@users.noreply.github.com>
2023-03-22 20:55:22 +0100

    main page welcome text added

--- frontend/components/Main.js
+++ frontend/components/Main.js
@@ -2,17 +2,34 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="flex  flex-col m-auto  object-center">
-      <div className=" flex justify-center object-center mx-10">
-        <img
-          src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
-          height={200}
-          width={1250}
-        ></img>
+    <div className="flex flex-col m-auto items-center">
+      <h1 className="text-5xl font-bold mb-8">Welcome to E-Buy!</h1>
+      <div className="flex flex-row justify-center mb-8">
+        <span className="text-3xl mr-4">🛍️</span>
+        <span className="text-3xl mr-4">🛒</span>
+        <span className="text-3xl mr-4">🎁</span>
+        <span className="text-3xl mr-4">👗</span>
+        <span className="text-3xl mr-4">👕</span>
+        <span className="text-3xl mr-4">👖</span>
+        <span className="text-3xl mr-4">👠</span>
+        <span className="text-3xl mr-4">💄</span>
+        <span className="text-3xl mr-4">🕶️</span>
       </div>
-      <div className="flex flex-col justify-center items-center"></div>
+      <p className="text-2xl text-center mx-40 mb-2">
+        Discover a world of fashion, beauty, and lifestyle products at your fingertips.
+      </p>
+      <p className="text-2xl text-center mx-40 mb-2">
+      Our online shopping site offers a wide range of high-quality items, from clothing and accessories to home decor and gifts.
+      </p>
+      <p className="text-2xl text-center mx-40 mb-8">
+      Shop with us from the comfort of your own home and enjoy the convenience of easy ordering and fast delivery.
+      </p>
+      <p className="text-2xl text-center mx-8">
+        Start browsing now and let us help you find the perfect items to suit your style and needs. Happy shopping!
+      </p>
     </div>
   );
 }
 
 export default Main;
+
